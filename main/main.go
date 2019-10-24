c39a72310848003d3b276af460a51f4ab7b714d4
Mufidzz <dmzmzzz@gmail.com>
2019-10-24 21:50:50 +0700

    Router Group added

--- main/main.go
+++ main/main.go
@@ -12,9 +12,33 @@ func main() {
 
 	router := gin.Default()
 
-	router.GET("/user/:id", inDB.GetUser)
-	router.GET("/user", inDB.GetUsers)
-	router.POST("/user", inDB.CreateUser)
+	user := router.Group("/user")
+	{
+		user.GET("/:id", inDB.GetUser)
+		user.GET("/", inDB.GetUsers)
+		user.POST("/", inDB.CreateUser)
+		user.PUT("/:id", inDB.UpdateUser)
+		user.DELETE("/:id", inDB.DeleteUser)
+		user.POST("/vote", inDB.Vote)
+	}
+
+	votingList := router.Group("/vl")
+	{
+		votingList.GET("/:id", inDB.GetVotingList)
+		votingList.GET("/", inDB.GetVotingLists)
+		votingList.POST("/", inDB.CreateVotingList)
+		votingList.PUT("/:id", inDB.UpdateVotingList)
+		votingList.DELETE("/:id", inDB.DeleteVotingList)
+	}
+
+	votingListItem := router.Group("/vli")
+	{
+		votingListItem.GET("/:id", inDB.GetVotingListItem)
+		votingListItem.GET("/", inDB.GetVotingListItems)
+		votingListItem.POST("/", inDB.CreateVotingListItem)
+		votingListItem.PUT("/:id", inDB.UpdateVotingListItem)
+		votingListItem.DELETE("/:id", inDB.DeleteVotingListItem)
+	}
 
 	err := router.Run(":7080")
 
